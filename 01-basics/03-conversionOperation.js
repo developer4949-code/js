c7deab6935ae3f98326f3cecf0390d1c4fffa87f
developer4949-code <dddebiprasaddas2004@gmail.com>
2025-06-08 02:00:08 +0000

    comparison and datatype summary done

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,54 @@
+let score="33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber)
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1 false=>0
+//null=>0
+//undefined=>NaN
+
+let isLoggedIn=1;
+let bolleanOfIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(bolleanOfIsLoggedIn)
+
+//1=>true ; 0=>false
+//""=>false; "debi"=>true
+
+let soemeNumber=143
+let stringNumber=String(soemeNumber)
+
+console.log(typeof stringNumber)
+console.log(stringNumber)
+
+//***********Operations**********
+
+let value=3
+let negValue=-value
+console.log(negValue)
+
+let str1="hello"
+let str2=" world"
+
+let str3=str1+str2
+
+console.log(str3)
+
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+let num1,num2,num3
+
+num1=num2=num3=34
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter)
