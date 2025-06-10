75b8a5d8fbd41f85a859c9c415ac195919fc9828
developer4949-code <dddebiprasaddas2004@gmail.com>
2025-06-10 20:14:12 +0000

    function started

---
+++ 03-basics/01-functions.js
@@ -0,0 +1,14 @@
+function saymyname()
+{
+    console.log("D")
+    console.log("E")
+    console.log("B")
+    console.log("I")
+}
+saymyname()
+
+function addtwonumber(num1,num2)
+{
+    return num1+num2;
+}
+console.log(addtwonumber(8,9))
