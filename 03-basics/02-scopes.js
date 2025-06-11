66ea8046e20d69250b9340ff767e80e76e1294d8
developer4949-code <dddebiprasaddas2004@gmail.com>
2025-06-11 06:37:25 +0000

    scope done(Block and Global)

---
+++ 03-basics/02-scopes.js
@@ -0,0 +1,18 @@
+// const a=10
+// let b=20
+// var c=30
+
+//{} scope
+
+if(true)
+{
+    const a=10
+    let b=20
+    var c=30//it does not have block scope so just avoid to use this one 
+}
+
+// console.log(a)
+// console.log(b)
+console.log(c)
+
+// Global scope is available for block scope but Block scope is not available for Global scope */
